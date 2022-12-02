7a57b186cc6c68fea4ce723b493f2fbfc6ec6b49
ROHAN <rohan.sasikumar@gmail.com>
2022-12-02 16:32:50 +0530

    ui mods

--- src/components/AllProjects.jsx
+++ src/components/AllProjects.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import Navbar from './Navbar'
+import Sidebar from './Sidebar'
 
 export default function AllProjects() {
   return (
-    <div>AllProjects</div>
+    <div className='allprojects'>
+      <Sidebar/>
+      <div className="allprojects-in">
+        <Navbar/>
+        <div>All Projects</div>
+      </div>
+    </div>
   )
 }
--- src/components/users/Profilepopup.jsx
+++ src/components/users/Profilepopup.jsx
@@ -1,6 +1,7 @@
 import React from "react";
+import Navbar from "../Navbar";
 
-export default function Profilepopup({ show,close }) {
+export default function Profilepopup({ show, close }) {
   return (
     <>
       {show ? (
@@ -8,11 +9,14 @@ export default function Profilepopup({ show,close }) {
           <div className="modal" onClick={(e) => e.stopPropagation()}>
             <header className="modal_header">
               <h2 className="modal_header-title"> Modal Title </h2>
-              
             </header>
-            <main className="modal_content">This is Modal Content</main>
+            <main className="modal_content">
+              Main
+            </main>
             <footer className="modal_footer">
-              <button className="modal-close" onClick={() => close()}>Cancel</button>
+              <button className="modal-close" onClick={() => close()}>
+                Cancel
+              </button>
 
               <button className="submit">Submit</button>
             </footer>
