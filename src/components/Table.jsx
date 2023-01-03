d16fd591df20579b99ad101fa6e80179dc706d21
jesi318 <jeswin.p.jaison@gmail.com>
2023-01-03 16:12:31 +0530

    fetched user table rows from firebase

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,20 +1,23 @@
 import { DataGrid } from "@mui/x-data-grid";
-import  React from "react";
+import React from "react";
 import "../scss/components/table.scss";
 import { useState } from "react";
 import Profilepopup from "./users/Profilepopup";
+import { useEffect } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../firebase";
 
 const columns = [
   { field: "id", headerName: "ID", width: 70 },
-  { field: "regno", headerName: "Register No", width: 100 },
-  { field: "Name", headerName: "Name", width: 150 },
+  { field: "reg_no", headerName: "Register No", width: 100 },
+  { field: "name", headerName: "Name", width: 150 },
   {
-    field: "class",
+    field: "classname",
     headerName: "Class",
     width: 90,
   },
   {
-    field: "groupid",
+    field: "grp_id",
     headerName: "Group ID",
     description: "This column has the Group ID",
     sortable: false,
@@ -22,33 +25,52 @@ const columns = [
   },
 ];
 
-const rows = [
-  {
-    id: 1,
-    regno: "2062025",
-    Name: "Jon",
-    class: "AIML",
-    groupid: "G2062025AIML",
-  },
-  {
-    id: 2,
-    regno: "2062025",
-    Name: "Jon",
-    class: "AIML",
-    groupid: "G2062025AIML",
-  },
-  {
-    id: 3,
-    regno: "2062025",
-    Name: "Jon",
-    class: "AIML",
-    groupid: "G2062025AIML",
-  },
-];
+// const rows = [
+//   {
+//     id: 1,
+//     regno: "2062025",
+//     Name: "Jon",
+//     class: "AIML",
+//     groupid: "G2062025AIML",
+//   },
+//   {
+//     id: 2,
+//     regno: "2062025",
+//     Name: "Jon",
+//     class: "AIML",
+//     groupid: "G2062025AIML",
+//   },
+//   {
+//     id: 3,
+//     regno: "2062025",
+//     Name: "Jon",
+//     class: "AIML",
+//     groupid: "G2062025AIML",
+//   },
+// ];
 
 const Table = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      let list = [];
+      try {
+        const querySnapshot = await getDocs(collection(db, "users"));
+        querySnapshot.forEach((doc) => {
+          list.push({ id: doc.id, ...doc.data() });
+        });
+
+        setData(list);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchData();
+  }, []);
   const [modal, setModal] = useState(false);
-  const Toggle = () => setModal(!modal);  
+  const Toggle = () => setModal(!modal);
   const actionColumn = [
     {
       field: "action",
@@ -57,10 +79,9 @@ const Table = () => {
       renderCell: () => {
         return (
           <div className="cellAction">
-            <div className="viewButton" onClick={()=>Toggle()}>
+            <div className="viewButton" onClick={() => Toggle()}>
               View
             </div>
-            
           </div>
         );
       },
@@ -72,14 +93,14 @@ const Table = () => {
         sx={{
           borderColor: "black",
         }}
-        rows={rows}
+        rows={data}
         getRowId={(rows) => rows.id}
         columns={columns.concat(actionColumn)}
         pageSize={8}
         rowsPerPageOptions={[8]}
         checkboxSelection
       />
-      <Profilepopup show={modal} title="My Modal" close={Toggle}/>
+      <Profilepopup show={modal} title="My Modal" close={Toggle} />
     </div>
   );
 };
