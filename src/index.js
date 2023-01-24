27ca69db2880f73fd5331280c9f546833d30fa69
ROHAN <rohan.sasikumar@gmail.com>
2023-01-24 10:44:57 +0530

    minor css changes

--- src/index.js
+++ src/index.js
@@ -2,6 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { AuthContextProvider } from "./context/AuthContext";
+import { useEffect } from "react";
+
+// const Zoom = () => {
+//   useEffect(() => {
+//     const initialValue = document.body.style.zoom;
+
+//     // Change zoom level on mount
+//     document.body.style.zoom = "90%";
+
+//     return () => {
+//       // Restore default value
+//       document.body.style.zoom = initialValue;
+//     };
+//   }, []);
+//   return <></>;
+// };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
